ddfea7cc62fc8edcbd09372d2d8022f07b6cbfb2
Ilja Smelich <ismelich@c2r6p7.hive.fi>
2020-10-06 13:15:45 +0300

    refactored error message

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const config = require('./config/config');
 const checkIfValidUrl = /^((http|https):\/\/)/;
 const monitorSession = [];
 
-config.websites.forEach(function (website) {
+const go = config.websites.forEach(function (website) {
+	console.log(
+		'\x1b[44m%s\x1b[0m',
+		'This monitoring sessions is about to begin! You can quit the programm with ctr + c'
+	);
 	if (!checkIfValidUrl.test(website.url))
 		return console.log('Not a valid url!');
-
 	const newRequest = new Monitor({
 		website: website.url,
 	});
